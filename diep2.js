a90624a55902179ea1e4562282fac1d5bc19b02b
Ernouf <48248463+Ernouf@users.noreply.github.com>
2020-03-10 09:53:35 +0100

    first commit

---
+++ diep2.js
@@ -0,0 +1,20 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+canvas.addEventListener("click",tir)
+
+var fps = 30;
+var IDsetInterval = setInterval(update, 1000/fps);
+
+function update(){
+    ctx.canvas.width  = window.innerWidth;
+    ctx.canvas.height = window.innerHeight;
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctx.fillStyle ="rgb(0,0,0)";
+    ctx.fillRect(10,10,20,20);
+}
+
+function tir(e){
+    console.log(e.screenX);
+    console.log(e.screenY);
+}
